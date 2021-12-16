1059d5c328fbdf590871e4a82105b16c158c323b
Justin Caovan <jtcaovan@gmail.com>
2021-12-15 21:31:44 -0700

    added custom themes and font to application

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,21 @@
+const defaultTheme = require('tailwindcss/defaultTheme');
+
 module.exports = {
-  content: [],
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
+    colors: {
+      'beaver': '#9E7F66',
+      'cod-gray': '#111111',
+      'mirage': '#17192B',
+      'ebony': '#242B37',
+      'shuttle-gray': '#5C6779'
+    },
+    fontFamily: {
+      sans: ['Spartan', ...defaultTheme.fontFamily.sans]
+    },
     extend: {},
   },
   plugins: [],
